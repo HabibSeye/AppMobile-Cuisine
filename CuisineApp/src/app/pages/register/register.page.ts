17f011753777a1baf4f4349a9f3b77d0eee81bbc
HabibSeye <habibseye505@gmail.com>
2021-04-12 19:18:52 +0200

    Register

--- CuisineApp/src/app/pages/register/register.page.ts
+++ CuisineApp/src/app/pages/register/register.page.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+
+
+import { UserRegister } from '../../interfaces/user-register';
+
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+  styleUrls: ['../login/login.page.scss', './register.page.scss'],
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  isErrorMail: boolean = true;
+    isErrorPhone: boolean = true;
+    user: UserRegister = { avatar: '', first_name: '', last_name: '', email: '', phone: '', password: '', confirm_password: '' };
 
-  ngOnInit() {
-  }
+  constructor(
+    private camera: Camera
+  ) { }
+
+  ngOnInit() {}
+  uploadPicture() {
+    const options: CameraOptions = {
+        quality: 100,
+        destinationType: this.camera.DestinationType.FILE_URI,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64 (DATA_URL):
+        let base64Image = 'data:image/jpeg;base64,' + imageData;
+        alert(imageData)
+    }, (err) => {
+        // Handle error
+        alert(err)
+    });
+}
 
 }
